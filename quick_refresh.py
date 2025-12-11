cd1a46761fa5aff0647e377eb73369d602ad1d96
sol4nki <solankipranjal44@gmail.com>
2025-12-11 16:06:58 +0530

    live reload improved

--- quick_refresh.py
+++ quick_refresh.py
@@ -5,18 +5,28 @@ import os, time
 import subprocess, sys
 
 file_loc = "./test.py"
-
-process = subprocess.Popen(["python3", file_loc])
-
+# i forgot to add sys.executable F
+process = subprocess.Popen([sys.executable, file_loc])
+last_mtime = os.path.getmtime(file_loc)
 while True:
     try:
-        last_mtime = os.path.getmtime(file_loc)
         time.sleep(1)
         curr_mtime = os.path.getmtime(file_loc)
         if curr_mtime != last_mtime:
-            process.terminate()
-            print("changes")
-            process = subprocess.Popen(["python3", file_loc])
+            try:
+                last_mtime = curr_mtime
+                process.terminate()
+                process.wait() # i am adding this because terminate doesnt wait [zombie process issue]
+                print("changes")
+            # this reload even works when eror but processlookuperror is still possible
+            except ProcessLookupError:
+                print("[!!!] IMPORTANT - somehow the process ended itself!?!??! issue with the test.py code fix errors")
+            process = subprocess.Popen([sys.executable, file_loc])
     except KeyboardInterrupt:
         print("[!!]= Progran exitting =[!!]")
+        try:
+            process.terminate()
+        except ProcessLookupError:
+            pass
+        process.wait()
         break
